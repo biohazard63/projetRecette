fb9c599b94db352b09c39849ec718f1368471aa3
Antoine <136310380+antoine-codeur@users.noreply.github.com>
2024-08-28 15:03:32 +0200

    1-login 💄 add style to login -image missing on left box -general css to index.css -header missing

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './login.css'
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -12,30 +12,37 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+      <main>
+      <div class="imgContainer">
+      
+      </div>
+      <div class="formWrapper">
+          <div class="formCard">
+          <h2>Fomulaire de connexion</h2>
+          <form onSubmit={handleSubmit}>
+              <div class="loginField">
+                <label>Email:</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+              </div>
+              <div class="loginField">
+                <label>Mot de passe:</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+              </div>
+              <button type="submit">Connexion</button>
+          </form>
+          </div>
+      </div>
+    </main>
   );
 }
 
